c0edad4f7d91ec0e924fc3fd1ce9ff23b4855ae2
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2025-06-22 17:57:06 +0200

    remove log search

--- Glouton/ViewModels/LogViewModel.cs
+++ Glouton/ViewModels/LogViewModel.cs
@@ -13,30 +13,15 @@ namespace Glouton.ViewModels;
 public class LogViewModel : BaseViewModel
 {
     private readonly ILoggingService _logger;
-    private string _searchText;
-    private readonly List<LogEntry> _logEntries;
-
-    public ObservableCollection<LogEntry> FilteredLogEntries { get; }
-
-    public string SearchText
-    {
-        get => _searchText;
-        set
-        {
-            _searchText = value;
-            OnPropertyChanged();
-            ApplyFilters();
-        }
-    }
+ 
+    public ObservableCollection<LogEntry> LogEntries { get; private set; }
 
     public ICommand ClearLogsCommand { get; }
 
     public LogViewModel(ILoggingService loggingService)
     {
         _logger = loggingService;
-        _searchText = string.Empty;
-        _logEntries = [];
-        this.FilteredLogEntries = new ObservableCollection<LogEntry>();
+        this.LogEntries = new ObservableCollection<LogEntry>();
         this.ClearLogsCommand = new ActionRelayCommand(ClearLogs);
 
         _logger.LogEntryAdded += OnLogEntryAdded;
@@ -44,35 +29,13 @@ public class LogViewModel : BaseViewModel
 
     private void OnLogEntryAdded(object? sender, LogEntryEventArgs arg)
     {
-        _logEntries.Add(arg.LogEntry);
-        ApplyFilters();
-    }
-
-    private void ApplyFilters()
-    {
-        FilteredLogEntries.Clear();
-
-        IEnumerable<LogEntry> filtered = _logEntries.Where(log =>
-        {
-            if (!string.IsNullOrEmpty(SearchText) && !log.Message.Contains(SearchText, StringComparison.OrdinalIgnoreCase))
-            {
-                return false;
-            }
-
-            return true;
-        });
-
-        foreach (LogEntry log in filtered)
-        {
-            FilteredLogEntries.Add(log);
-        }
+        LogEntries.Add(arg.LogEntry);
     }
 
     private void ClearLogs()
     {
         _logger.ClearLogs();
-        _logEntries.Clear();
-        FilteredLogEntries.Clear();
+        LogEntries.Clear();
     }
 
     protected override void Dispose(bool disposing)
@@ -81,4 +44,3 @@ public class LogViewModel : BaseViewModel
         base.Dispose(disposing);
     }
 }
-
--- Glouton/Views/LogView.xaml.cs
+++ Glouton/Views/LogView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Glouton.Views
 {
@@ -7,9 +10,83 @@ namespace Glouton.Views
     /// </summary>
     public partial class LogView : UserControl
     {
+        private bool _isScrolling;
+
         public LogView()
         {
             InitializeComponent();
+            this.DataContextChanged += LogView_DataContextChanged;
+            this.Unloaded += LogView_Unloaded;
+        }
+        
+        private void LogView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue is ViewModels.LogViewModel oldViewModel && 
+                oldViewModel.LogEntries is INotifyCollectionChanged oldCollection)
+            {
+                oldCollection.CollectionChanged -= FilteredLogEntries_CollectionChanged;
+            }
+            
+            if (e.NewValue is ViewModels.LogViewModel newViewModel && 
+                newViewModel.LogEntries is INotifyCollectionChanged newCollection)
+            {
+                newCollection.CollectionChanged += FilteredLogEntries_CollectionChanged;
+            }
+        }
+        
+        private void FilteredLogEntries_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_isScrolling)
+            {
+                return;
+            }
+                
+            if (e.Action == NotifyCollectionChangedAction.Add || e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                if (logDataGrid.Items.Count > 0)
+                {
+                    try
+                    {
+                        _isScrolling = true;              
+                        logDataGrid.Dispatcher.BeginInvoke((Action)(() => 
+                        {
+                            try
+                            {
+                                if (logDataGrid.Items.Count > 0)
+                                {
+                                    var lastIndex = logDataGrid.Items.Count - 1;
+                                    logDataGrid.ScrollIntoView(logDataGrid.Items[lastIndex]);
+                                }
+                            }
+                            finally
+                            {
+                                _isScrolling = false;
+                            }
+                        }), System.Windows.Threading.DispatcherPriority.Background);
+                    }
+                    catch
+                    {
+                        _isScrolling = false;
+                        throw;
+                    }
+                }
+                else
+                {
+                    _isScrolling = false;
+                }
+            }
+        }
+
+        private void LogView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ViewModels.LogViewModel viewModel &&
+                viewModel.LogEntries is INotifyCollectionChanged collection)
+            {
+                collection.CollectionChanged -= FilteredLogEntries_CollectionChanged;
+            }
+
+            this.DataContextChanged -= LogView_DataContextChanged;
+            this.Unloaded -= LogView_Unloaded;
         }
     }
 }
